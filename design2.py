1218e13d9f36d05cd14833e971c77868ec2a3821
Sonu Swapan Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2022-01-22 20:14:02 +0530

    Add files via upload

---
+++ design2.py
@@ -0,0 +1,18 @@
+
+import turtle
+import colorsys
+
+t=turtle.Turtle()
+s=turtle.Screen()
+s.bgcolor('black')
+t.speed(0)
+n=200
+h=0
+for i in range(360):
+    c=colorsys.hls_to_rgb(h,1,0.6)
+    h+=1/n
+    t.color(c)
+    for j in range(2):
+        t.left(100)
+        t.forward(5*i**j)
+        
