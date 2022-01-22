bab28ccf6f2203daff9a81121f12eed58378dd3f
Sonu Swapan Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2022-01-22 20:15:28 +0530

    Add files via upload

---
+++ Print_I_.py
@@ -0,0 +1,17 @@
+import turtle
+t=turtle.Turtle()
+
+t.pensize(10)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(25)
+t.back(50)
+t.forward(25)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(25)
+t.back(50)
+
+turtle.done()
